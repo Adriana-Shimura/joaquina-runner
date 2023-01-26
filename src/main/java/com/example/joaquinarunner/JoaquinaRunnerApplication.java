33b3701b5c640f1b0c24e3c4119080e1f7c93792
Adriana-Shimura <120142371+Adriana-Shimura@users.noreply.github.com>
2023-01-26 20:02:09 -0300

    first commit

---
+++ src/main/java/com/example/joaquinarunner/JoaquinaRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.joaquinarunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaquinaRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaquinaRunnerApplication.class, args);
+	}
+
+}
